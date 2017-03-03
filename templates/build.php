0c191f9cb2a76df437a15c8dc46acc1a06dcb45b
Louis Eveillard <louis.eveillard@gmail.com>
2017-03-03 11:25:59 +0100

    renamed all pages in menu

--- templates/build.php
+++ templates/build.php
@@ -4,11 +4,11 @@
     $currentPage = basename($_SERVER["SCRIPT_FILENAME"], '.php');
     $allPages = array(
       'index' => 'Home',
-      'web-basics' => '1. Web Basics',
-      'content'=> '2. Content',
-      'presentation' => '3. Presentation',
-      'layout' => '4. Layout',
-      'multipage_and_CMS' => '5. Dynamic pages',
+      'web-basics' => '1.Web Basics',
+      'content'=> '2.Content',
+      'presentation' => '3.Presentation',
+      'layout' => '4.Layout',
+      'multipage_and_CMS' => '5.Dynamic pages',
       'patterns' => 'Patterns',
       'references' => 'References',
       'formatting' => '- formatting',
